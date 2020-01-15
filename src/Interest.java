1e3d8972e1a45511869d94508cf97ce83c02175a
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-15 03:13:56 +0100

    interest rate

---
+++ src/Interest.java
@@ -0,0 +1,16 @@
+//using the for statement, call the calculate Interest method
+// with amount of 10000 with an interest rate of 2,3,4,5,6,7 and 8
+
+
+public class Interest {
+    public static void main(String[] args) {
+        for (int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i +" 2% interest = " +  calculateInterest(10000.0,i));
+        }
+    }
+
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+}

75309ed6fba035616a23d19a6d5e6ecd133df0dc
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-19 13:00:29 +0100

    Display numbers

---
+++ src/Display.java
@@ -0,0 +1,9 @@
+// DISPLAY NUM FROM 1 - 10
+
+public class Display {
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 10; counter ++) {
+            System.out.printf("%d. %n ", counter);
+        }
+    }
+}

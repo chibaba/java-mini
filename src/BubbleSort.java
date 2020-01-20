fae373d0558e9cb48e59395aa8ea3ea959e09159
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-20 23:03:55 +0100

    experimented bubble array

---
+++ src/BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int [] intArray = {12, 1, 87, 98, 9, 89, 34};
+
+        for (int lastUnsortedIndex =intArray.length -1; lastUnsortedIndex > 0;
+              lastUnsortedIndex--) {
+            for (int i =0; i < lastUnsortedIndex; i++) {
+                if(intArray[i] > intArray[i + 1]) {
+                    swap(intArray, i, i +1);
+                }
+            }
+        }
+
+    }
+    public static void swap(int [] array, int i, int j) {
+        if ( i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}

2aa8843f6cff39aa2ac222ec5b08a7f903cff586
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-20 19:09:17 +0100

    started array

--- src/Sum.java
+++ src/Sum.java
@@ -1,2 +1,12 @@
+ // SUM OF THE FIRST 20 INT NUMBERS
 public class Sum {
+    public static void main(String[] args) {
+        int sum = 0;
+//        int counter = 0;
+        for (int counter = 0; counter <= 20; counter++) {
+            if (counter % 2 == 0)
+                sum +=counter;
+            System.out.printf("Sum is %d%n ", sum);
+        }
+    }
 }
---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int [] intArray = new int[7];
+
+        intArray[0] = 39;
+        intArray[1] = -12;
+        intArray[2] = 3;
+        intArray[3]  = 34;
+        intArray[4] = -34;
+        intArray[5] = 24;
+        intArray[6] = 53;
+         for (int i = 0; i < intArray.length; i++) {
+             System.out.println(intArray[i]);
+         }
+    }
+}

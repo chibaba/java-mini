d446f242e43067322204252858899b39dcf84c1b
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-20 21:43:14 +0100

    selecting from array

--- src/Test.java
+++ src/Test.java
@@ -10,8 +10,18 @@ public class Test {
         intArray[4] = -34;
         intArray[5] = 24;
         intArray[6] = 53;
-         for (int i = 0; i < intArray.length; i++) {
-             System.out.println(intArray[i]);
-         }
+//         for (int i = 0; i < intArray.length; i++) {
+//             System.out.println(intArray[i]);
+//         }
+         // assuming the previous dosent exist we want to get the no 7 from the array
+           int index = -1;
+        for ( int i = 0; i  < intArray.length; i++) {
+            if (intArray[i] == 24)  {
+                index = i;
+                break;
+            }
+        }
+        System.out.println("index = " + index);
+
     }
 }
